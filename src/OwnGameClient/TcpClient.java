f9be05fdc859782c595b4a4c169ba0688726323f
cshiz9206 <cshiz9206@gamil.com>
2022-02-16 14:11:51 +0900

    Feat version.03.01
    Resolves :
       - end of Tcp (2 player)
       - end of DB (with client connect)
       - end of play mode (1p or 2p you can choose)
    
    in process :
       - add image path class
       - separate commonness class and distinct class to package
       - refactoring (OOP)
       - more player

---
+++ src/OwnGameClient/TcpClient.java
@@ -0,0 +1,107 @@
+package OwnGameClient;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class TcpClient extends Thread {
+	Socket clientSocket;
+	BufferedReader in;
+	static BufferedWriter out;
+//	ObjectInputStream in;
+//	ObjectOutputStream out;
+	int[] data;
+	
+	final int PORT = 9999;
+	final String HOSTIP = "210.102.142.30";
+	
+	Bar barClient;
+	Ball ball;
+	int frameX, frameY;
+	
+	static boolean allConnected = false;
+	
+	public TcpClient(Bar barClient, Ball ball, int frameX, int frameY) {
+		try {
+			System.out.println(InetAddress.getLocalHost().getHostAddress()); //https://onmay.tistory.com/18
+			clientSocket = new Socket(HOSTIP, PORT);
+			System.out.println("서버와 연결이 되었습니다.\n");
+			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+//			out = new ObjectOutputStream(clientSocket.getOutputStream());
+//			in = new ObjectInputStream(clientSocket.getInputStream());
+		} catch (NumberFormatException | IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println();
+			e.printStackTrace();
+		}
+		
+		this.barClient = barClient;
+		this.ball = ball;
+		this.frameX = frameX;
+		this.frameY = frameY;
+	}
+	
+	public void run() {
+		data = new int[6];
+		while(true) {
+			try {
+				if(!clientSocket.isConnected()) break;
+				String tmp = in.readLine(); // line 중단점(\n) 주의
+				//System.out.println(tmp);
+				
+				if(tmp.contentEquals("저장완료")) {
+					BreakOutFrame.saveBtn.setEnabled(false);
+					new SavedInfoFrame(frameX, frameY);
+					BreakOutFrame.dataUpdate();
+				}
+				else if(tmp != null) {
+					//System.out.println(tmp.split(",")[0]);
+					data[0] = Integer.parseInt(tmp.split(",")[0]); // bar
+					data[1] = Integer.parseInt(tmp.split(",")[1]); // bar moveLeftAmt
+					data[2] = Integer.parseInt(tmp.split(",")[2]); // bar moveRightAmt
+					data[3] = Integer.parseInt(tmp.split(",")[3]); // ball x
+					data[4] = Integer.parseInt(tmp.split(",")[4]); // ball y
+					data[5] = Integer.parseInt(tmp.split(",")[5]);
+					
+					if(data[0] == -1) {
+						allConnected = true;
+					}
+					else {
+						barClient.moveByConnect(data[0], data[4]);
+						if(data[1] != -1) barClient.setMovLeftAmt(data[1]);
+						if(data[2] != -1) barClient.setMovRightAmt(data[2]);
+						ball.moveByConnect(data[3], data[4]);
+						if(data[5] != -1) ball.crash(data[5]);
+					}
+				}
+				
+//				if(!allConnected) {
+//					clientSocket.close();
+//					break; 
+//				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void sendMsg(String data) {
+		try {
+			//System.out.println(barX + "," + ballX + "," + ballY);
+			out.write(data);
+			out.newLine();
+			out.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/OwnGameServer/TcpServer.java
@@ -0,0 +1,58 @@
+package OwnGameServer;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
+public class TcpServer extends Thread {
+	static ArrayList<ClientConnection> clientConnections;
+	ServerSocket listener;
+	final int clientMax = 2;
+	int connectedCnt = 0;
+	
+	public TcpServer(int port) {
+		clientConnections = new ArrayList<ClientConnection>();
+		try {
+			listener = new ServerSocket(port);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		while(true) {
+			try {
+				if(ServerFrame.isDisposed) break;
+				
+				for(ClientConnection cc : clientConnections) {
+					if(!cc.isAlive()) {
+						cc = null;
+					}
+				}
+				clientConnections.add(new ClientConnection(connectedCnt + 1, this, listener.accept()));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			clientConnections.get(connectedCnt).start();
+			connectedCnt += 1;
+			if(connectedCnt % clientMax == 0) sendMsg(-1, "-1,-1,-1,-1,-1,-1");
+		}
+	}
+	
+	public synchronized void sendMsg(int sentNo, String data) {
+		
+		// 현재 server에 들어온 데이터를 보낸 클라이언트와 나머지 클라이언트 구분하여 데이터 전송
+		for(int i = 0; i < connectedCnt; i++) {
+			if(i == (sentNo - 1)) {
+				//System.out.println(i);
+				continue;
+			}
+			//System.out.println(i);
+			clientConnections.get(i).sendMsg(data);
+		}
+		
+		if(sentNo >= 0) clientConnections.get(sentNo - 1).setIsSendNow();
+	}
+}
